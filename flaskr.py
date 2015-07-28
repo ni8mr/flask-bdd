3a48f92e8dda4cd8d9c98e845c8b6acfd6659c13
ni8mr <nilpaddo@gmail.com>
2015-07-28 16:02:57 +0600

    Adding login() and logout() function

--- flaskr.py
+++ flaskr.py
@@ -51,6 +51,27 @@ def add_entry():
     flash('New entry was successfully posted.')
     return redirect(url_for('show_entries'))
 
+@app.route('/login', methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username.'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid username.'
+        else:
+            session['logged_in'] = True
+            flash('You are logged in.')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out.')
+    return redirect(url_for('show_entries'))
+
 
 
 
