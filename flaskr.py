35dc625042fba1e11677b212bad56e0fd96429a9
ni8mr <nilpaddo@gmail.com>
2015-07-28 12:25:46 +0600

    More advancement

--- flaskr.py
+++ flaskr.py
@@ -2,6 +2,7 @@
 import sqilte3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+from contextlib import closing
 
 
 # configuration
@@ -17,6 +18,14 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
 
 if __name__ == '__main__':
     app.run()
