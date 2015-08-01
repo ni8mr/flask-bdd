ccc083e95f4583dcee9cb54c582ac3a37b415622
ni8mr <nilpaddo@gmail.com>
2015-08-01 10:40:32 +0600

    Added features, steps and source codes according to the steps. Still failing though

--- flaskr.py
+++ flaskr.py
@@ -1,10 +1,37 @@
-from flask import Flask
+# imports
+from flask import Flask, render_template, request, session
+
+# configuration
+DATABASE = ''
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'bla_bla'
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	"""User login/authentication/session management."""
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('index'))
+	return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+	"""User logout/authentication/session management."""
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
